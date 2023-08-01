e17f30284f896ade30f3dc9c970c5a57fe695c21
AlexSup21 <alexszczepanski08@gmail.com>
2023-08-01 13:11:55 +0200

    Create setup script. Added qutebrowser, lf, etc. Changed xbindkeys.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,19 @@
 
 repo="$HOME/dotfiles"
 
-echo "Installing packages"
-sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort $repo/packages.list))
+function install_q {
+    while true; do
+        read -p "$* Install packages from list? [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
+
+cat $repo/packages.list
+
+install_q && sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort $repo/packages.list))
 
 echo "Creating symlinks"
 ln -sf $repo/.bashrc ~
@@ -21,3 +32,7 @@ ln -sf $repo/mpv ~/.config
 ln -sf $repo/neofetch ~/.config
 ln -sf $repo/nvim ~/.config
 ln -sf $repo/vlc ~/.config
+ln -sf $repo/qutebrowser ~/.config
+ln -sf $repo/lf ~/.config
+
+echo "Done!"
